c570f53578f36bb80beadd0e44283b2e8a50cc46
hariadiarief <adi.arief77@gmail.com>
2020-08-15 20:25:15 +0700

    error

--- main.go
+++ main.go
@@ -2,15 +2,50 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-  )
+	"github.com/jinzhu/gorm"
 
+	"fmt"
 
-func main()  {
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type City struct {
+	gorm.Model
+	ID   int
+	Name string
+}
+
+func main() {
+
 	r := gin.Default()
+	db, err := gorm.Open("mysql", "root@/doogether_legacy_region")
+
+	if err != nil {
+		fmt.Panic(err)
+	}
+
+	defer db.Close()
+
 	r.GET("/cuk", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
+
+		rows, err := db.Raw("SELECT ID, name FROM city").Rows()
+		defer rows.Close()
+
+		cities := make([]City, 0)
+
+		for rows.Next() {
+
+			var city City
+
+			rows.Scan(&city.ID, &city.Name)
+
+			cities = append(cities, city)
+		}
+
+		c.SecureJSON(200, gin.H{
+			"data": cities,
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
 }
